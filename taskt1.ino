57e28bdee6864f44026428db0f5c21262a9a19a4
Kartik Arora <90651659+KartikAroraOfficial@users.noreply.github.com>
2023-02-22 22:58:17 +0530

    Update taskt1.ino

--- taskt1.ino
+++ taskt1.ino
@@ -5,7 +5,7 @@ const byte temperature = A2;
 
 void setup()
 {
-  pinMode(13, OUTPUT);
+  pinMode(11, OUTPUT);
   pinMode(temperature, INPUT);
   Serial.begin(9600);
 }
@@ -23,10 +23,10 @@ void loop()
   Serial.print("Current Temperature: ");
   Serial.println(temp);
   
-  if(temp>20){
-  	digitalWrite(13, HIGH);
+  if(temp>30){
+  	digitalWrite(11, HIGH);
     delay(1000);
-    digitalWrite(13, LOW);
+    digitalWrite(11, LOW);
     
   }
 }
